24f6194cc20cc5702c7bbb7af9b7e9e45501597b
dest7 <Edward.dest@yandex.ru>
2016-08-01 20:23:52 +0400

    Задание 3.
    Добавил тест работы функции по расчету расстояния между двумя точками.

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
@@ -11,10 +11,15 @@ public class DistanceTest {
     public void testDistance(){
         Point p1 = new Point(1.5,-2.3);
         Point p2 = new Point(1,3.6);
-        assert p1.distance(p2)==5.921148537234985;
-        Assert.assertEquals(p1.distance(p2),5.921148537234985);
-        Assert.assertEquals(p1.distance(p2),Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2)));
+        
+        //Assert.assertEquals(p1.distance(p2),5.921148537234985);
+        Assert.assertEquals(p1.distance(p2),distanceMath(p1,p2));
+
         //5.921148537234985
         //Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2))
     }
+    public static double distanceMath(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
+
+        }
 }
