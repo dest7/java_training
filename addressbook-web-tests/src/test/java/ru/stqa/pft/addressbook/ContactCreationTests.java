c86348cddd6dd1e3df365a4a3dba637d1a829a90
dest7 <Edward.dest@yandex.ru>
2016-08-01 23:27:04 +0400

    Домашнее задание №4. Тест создания контакта
    Изменил название тестового метода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -31,7 +31,7 @@ public class ContactCreationTests {
     }
 
     @Test
-    public void ContactCreationTests() {
+    public void testContactCreation() {
 
         addNewContact();
         fillContactDataForm(new ContactData("Ed", "JavaTest", "Moscow City", "8-495-1231231", "adressbook@abmail.com"));
