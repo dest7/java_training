47c6fac3301479493c6ba4f469bf36d4870059c9
dest7 <Edward.dest@yandex.ru>
2016-08-02 23:52:35 +0400

    Сформировал тест для удаления контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
@@ -3,16 +3,11 @@ package ru.stqa.pft.addressbook;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class ContactDeletionTests {
     FirefoxDriver wd;
@@ -21,21 +16,26 @@ public class ContactDeletionTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
     }
-    
-    @Test
-    public void ContactDeletionTests() {
-        wd.get("http://localhost/addressbook/");
+
+    private void login(String login, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(login);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testContactDeletion() {
         wd.findElement(By.linkText("home")).click();
+        wd.findElement(By.xpath("//tr[3]/td/input")).click();
         wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
-        wd.findElement(By.linkText("group page")).click();
+        wd.switchTo().alert().accept();
     }
     
     @AfterMethod
