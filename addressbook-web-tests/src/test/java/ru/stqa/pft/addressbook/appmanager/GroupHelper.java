d2c6a8dd0081b2d845784b07d8a8cf673d8f033c
dest7 <Edward.dest@yandex.ru>
2016-08-20 17:05:58 +0300

    l4_m6
    Сравнение списков целиком, до удаления и после удаления.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -42,7 +42,6 @@ public class GroupHelper extends BaseHelper {
 
     public void selectGroup(int index) {
         wd.findElements(By.name("selected[]")).get(index).click();
-        click(By.name("selected[]"));
     }
 
     public void initGroupModification() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -13,7 +13,7 @@ public class GroupDeletionTests extends TestBase {
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
-        app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
+            app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
@@ -21,6 +21,10 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
+
     }
 
 }
