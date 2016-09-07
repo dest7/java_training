f0599e7358468405a0698a0ce67f6f05580efc1c
dest7 <Edward.dest@yandex.ru>
2016-09-07 21:09:44 +0300

    Хэширование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -34,8 +34,8 @@ public class GroupModificationTests extends TestBase{
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("testMod1").withHeader("testMod2").withFooter("testMod3");
         app.group().modify(group);
+        assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.group().all();
-        assertEquals(after.size(), before.size());
         assertThat(after, equalTo(before.withOut(modifiedGroup).withAdded(group)));
 
     }
