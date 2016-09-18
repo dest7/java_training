917ea70c03ca63dfcc5f3bcb6a0192acc7ffbd07
dest7 <Edward.dest@yandex.ru>
2016-09-19 01:51:28 +0300

    14 и 15 задание.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,21 +1,38 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 
+@XStreamAlias("contact")
 public class ContactData {
+
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
+    @Expose
     private  String firstName;
+    @Expose
     private  String lastName;
+    @Expose
     private  String address;
+    @Expose
     private  String homePhone;
+    @Expose
     private  String mobilePhone;
+    @Expose
     private  String workPhone;
-    private  String allPhones;
-    private  String group;
+    @Expose
     private  String email;
-    private  String allEmail;
+    @Expose
     private  String emailTwo;
+    @Expose
     private  String emailThree;
+    private  String allEmail;
+    private  String allPhones;
+    @Expose
+    private  String group;
     private  String dataFromDetailPage;
     private  String allData;
     private File photo;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,24 +1,66 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-    @Test //(enabled = false)
-    public void testContactCreation() {
 
-        String[] names = new String[] {"Test1", "Test2", "Test3"};
-        for (String name : names){
+    @DataProvider //провайдер тестовых данных
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
 
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json += line;
+            line = reader.readLine();
         }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); //List<ContactData>.class
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+
+
+    @Test (dataProvider = "validContactsFromJson")//(enabled = false)
+    public void testContactCreationUseJson(ContactData contact) {
+
+//        app.goTo().groupPage();
+//        if (! app.group().isThereAGroup()){
+//            app.group().create(new GroupData().withName("Test1").withHeader("Test2").withFooter("Test3"));
+//        }
+        app.goTo().homePage();
+        Contacts before = app.contact().all();
+        app.contact().createContact(contact);
+        assertThat(app.contact().count(), equalTo(before.size() + 1));
+        Contacts after = app.contact().all();
+        assertThat(after, equalTo(
+                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    }
+
+
+    @Test //(enabled = false)
+    public void testContactCreation (){
+
         app.goTo().groupPage();
         if (! app.group().isThereAGroup()){
             app.group().create(new GroupData().withName("Test1").withHeader("Test2").withFooter("Test3"));
