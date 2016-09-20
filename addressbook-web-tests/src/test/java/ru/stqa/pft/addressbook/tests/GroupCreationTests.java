8344bf309166ee5db7a423ad68e9dd62b300c04f
dest7 <Edward.dest@yandex.ru>
2016-09-21 00:28:57 +0300

    Логирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -21,8 +23,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-
-    @DataProvider //провайдер тестовых данных
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
         try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))){
             String xml = "";
@@ -55,6 +55,7 @@ public class GroupCreationTests extends TestBase {
 
     @Test (dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
+
             app.goTo().groupPage();
             Groups before = app.group().all();
             app.group().create(group);
@@ -62,6 +63,7 @@ public class GroupCreationTests extends TestBase {
             Groups after = app.group().all();
             assertThat(after, equalTo(
                     before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+            logger.info("Stop testGroupCreation");
     }
 
     @Test
