2dc34b3bd816203c87ba2c6eadb5ecbc2d700602
dest7 <Edward.dest@yandex.ru>
2016-10-21 17:12:58 +0300

    добавить крипто-шифр

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -7,3 +7,4 @@ $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
+$g_crypto_master_salt     = 'jYp3L3W09Y4MLYNGPeUiP+n8ifVxoRlASx+EYxfvIVg=';
